7e8dc80cf681788c379b32498454980aef9e64b2
bianca666 <32862522+bianca666@users.noreply.github.com>
2019-02-21 12:10:53 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,32 @@
-$('body').scrollspy()
+(function(){
+	var init = function() {
+		var updateOrientation = function() {
+			var orientation = window.orientation;
+			var ori;
+			switch(orientation){
+				case 90:
+				case -90:
+					ori = 1;	/*landscape*/
+					break;
+				default:
+					ori = 2;  /*portrait*/
+					break;
+			}
+
+		}
+		
+		var homeInner = document.getElementsByClassName('home-inner')[0];
+		var width = document.body.clientWidth;
+		if(width == 1024 && ori == 1){
+			homeInner.style.background = 'url(../imgs/landingbg.jpg)';
+		}
+
+		console.log(orientation, ori, homeInner.style.background);
+		/*../imgs/landing_md.jpg*/
+		window.addEventListener('orientationchange', updateOrientation, false);
+	}
+
+	 window.addEventListener("DOMContentLoaded",init,false);  
+	
+})();
+
